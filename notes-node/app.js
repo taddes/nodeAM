2b50f393e8bad1d6d1a2c78d534bad54a733a155
Taddes Korris <taddes.korris@gmail.com>
2018-10-21 23:10:08 -0400

    args and yargs

--- notes-node/app.js
+++ notes-node/app.js
@@ -2,19 +2,53 @@ console.log('Starting app.js');
 
 const fs = require('fs');
 const os = require('os');
-const notes = require('./notes')
+
+const _ = require('lodash');
+const yargs = require('yargs');
+
+const notes = require('./notes');
 
 // OS Module
 let user = os.userInfo();
 console.log({user})
 
+
+console.log(_.isString('ladeda'));
+console.log(_.isString(true));
+
+let filteredArray = _.uniq([2, 6, 8, 12, 33, 8, 6, 33, 24])
+console.log({filteredArray})
+
+const compare = process.argv;
+
+const argv = yargs.argv;
+console.log({argv, compare})
+
+
 // FS Module
-fs.appendFile('greetings.txt', `Hello there ${user.username} `, function Error(err) {
-  if (err) {
-    console.log(`Unable to write to file: ${err}`);
-  } else {
-    console.log('Completed appending the file');
-  }
-});
+// fs.appendFile('greetings.txt', `Hello there ${user.username}! You are ${notes.age} `, function Error(err) {
+//   if (err) {
+//     console.log(`Unable to write to file: ${err}`);
+//   } else {
+//     console.log('Completed appending the file');
+//   }
+// });
+
+let command = process.argv[2];
+console.log(`Command ${command}`)
+
+console.log(process.argv);
+
+if (command === 'add') {
+  notes.addNote(argv.title, argv.body)
+} else if (command === 'list') {
+  notes.getAll();
+} else if (command ==='read') {
+  notes.readNote(argv.title)
+} else if (command === 'remove') {
+  notes.removeNote(argv.title)
+ } else {
+  console.log('Command not recognized')
+}
 
 
--- notes-node/notes.js
+++ notes-node/notes.js
@@ -1,1 +1,19 @@
-console.log('Starting notes.js')
+console.log('Starting notes.js');
+
+let addNote = (title, body) => {
+  console.log(`Adding Note = title: ${title}. body: ${body}.`)
+}
+
+let getAll = () => {
+  console.log('Getting all notes');
+}
+
+let readNote = (note) => {
+  console.log(`Reading Note: ${note}`)
+}
+
+let removeNote = (note) => {
+  console.log(`Removing note: ${note}`)
+}
+
+module.exports = {addNote, getAll, readNote, removeNote}
